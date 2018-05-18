e4fc3fe024fb861a887ab727678fa236483fc1a8
Tuong Van Nguyen <tvan262@yahoo.com>
2018-05-18 17:45:20 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,32 @@
 # code here!
 
 class School
-  
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
   def roster
- end
- 
- hash[new_key] = []
- hash[new_key] << new_value_for_value_array
- 
-hash
- => {"new_key"=>["new_value_for_value_array"]}
- 
+    @roster
+  end
+
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
